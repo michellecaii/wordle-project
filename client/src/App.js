c83e1e188aa001982a7b9e887ad1907222a82a95
michellecaii <mc8870@nyu.edu>
2025-04-19 13:47:08 -0400

    Update App.js
    Limit the guesses to 6 + implement win/loss condition

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,10 @@ import "./App.css";
 function App() {
   const [guess, setGuess] = useState("");
   const [guesses, setGuesses] = useState([]); // array of { word: string, feedback: string[] }
-
+  const [gameStatus, setGameStatus] = useState("inProgress");
 
   const submitGuess = async () => {
-    if (guess.length !== 5) return;
+    if (guess.length !== 5 || gameStatus !== "inProgress") return;
 
     const response = await fetch("http://localhost:5050/guess", {
       method: "POST",
@@ -16,8 +16,18 @@ function App() {
     });
 
     const data = await response.json();
+    const newEntry = { word: guess, feedback: data.result };
+    const newGuesses = [...guesses, newEntry];
+
     setGuesses([...guesses, { word: guess, feedback: data.result }]);
     setGuess(""); // reset input
+
+    //Check win condition
+    if (data.result.every((color) => color === "green")){
+      setGameStatus("won");
+    } else if (newGuesses.length >= 6){
+      setGameStatus("lost");
+    }
   };
 
   return (
@@ -49,6 +59,15 @@ function App() {
           </div>
         ))}
       </div>
+      {gameStatus === "won" && (
+      <h2 style={{ color: "green", marginTop: "1rem" }}>Good job! You guessed it!</h2>
+    )}
+    {gameStatus === "lost" && (
+      <h2 style={{ color: "red", marginTop: "1rem" }}>
+        Sorry, better luck next time. The word was: PARTY
+      </h2>
+    )}
+
     </div>
   );
 }
